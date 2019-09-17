9268a97fb7459b8becb1f21dd75ed42458c1dd5a
test <test@gmail.com>
2019-09-18 00:01:00 +0300

    build lilv

--- default.nix
+++ default.nix
@@ -89,6 +89,27 @@ let
     };
   };
 
+  lilv = stdenv.mkDerivation rec {
+    pname = "lilv";
+    version = "0.24.4";
+
+    src = fetchurl {
+      url = "https://download.drobilla.net/${pname}-${version}.tar.bz2";
+      sha256 = "0f24cd7wkk5l969857g2ydz2kjjrkvvddg1g87xzzs78lsvq8fy3";
+    };
+
+    nativeBuildInputs = [ pkgconfig wafHook ];
+    buildInputs = [ lv2 python serd sord sratom ];
+
+    meta = with stdenv.lib; {
+      homepage = http://drobilla.net/software/lilv;
+      description = "A C library to make the use of LV2 plugins";
+      license = licenses.mit;
+      maintainers = [ maintainers.goibhniu ];
+      platforms = platforms.linux;
+    };
+  };
+  
   vampSDK = stdenv.mkDerivation {
     name = "vamp-sdk-2.7.1";
     # version = "2.7.1";
@@ -204,7 +225,7 @@ let
       libuuid 
       libxml2 
       libxslt 
-      # lilv 
+      lilv 
       lv2
       makeWrapper 
       pango 
